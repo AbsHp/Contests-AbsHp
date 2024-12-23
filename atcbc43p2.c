b33d9c908451aa4a474307b57797a533256aac89
AbsHp <asinghal21@iitk.ac.in>
2024-12-24 00:36:49 +0530

    atcbc43p2

--- atcbc43p2.c
+++ atcbc43p2.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_LEN 15
 
 int main()
 {
+    char s[MAX_LEN + 1];
+    char result[MAX_LEN + 1];
+    int resultIndex = 0;
+    scanf("%s", s);
+    int len = strlen(s);
+    for (int i = 0; i < len; i++)
+    {
+        if (s[i] == '0' || s[i] == '1')
+        {
+            result[resultIndex++] = s[i];
+        }
+        else if (s[i] == 'B')
+        {
+            if (resultIndex > 0)
+            {
+                resultIndex--;
+            }
+        }
+    }
+    result[resultIndex] = '\0';
+    printf("%s\n", result);
     return 0;
 }
+
+// AtCoder Beginner Contest 43 P2
