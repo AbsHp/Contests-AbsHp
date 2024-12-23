0085330e2d81927756beb9b03c603dc7e8f55c8d
AbsHp <asinghal21@iitk.ac.in>
2024-12-23 23:47:04 +0530

    c8

---
+++ atcbc385p3.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int maxBuildings(int heights[], int n)
+{
+    int maxCount = 1;
+    for (int h = 0; h < n; h++)
+    {
+        for (int interval = 1; interval < n; interval++)
+        {
+            int count = 1;
+            int prevIndex = h;
+            for (int i = h + interval; i < n; i += interval)
+            {
+                if (heights[i] == heights[prevIndex])
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (count > maxCount)
+            {
+                maxCount = count;
+            }
+        }
+    }
+    return maxCount;
+}
+
+int main()
+{
+    int n;
+    printf("Enter the number of buildings: ");
+    scanf("%d", &n);
+    int heights[n];
+    printf("Enter the heights of the buildings: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &heights[i]);
+    }
+    int result = maxBuildings(heights, n);
+    printf("%d\n", result);
+    return 0;
+}
