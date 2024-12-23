8fc27b7061a56a7134d65c7ad6424b43213e3e4b
AbsHp <asinghal21@iitk.ac.in>
2024-12-23 23:47:27 +0530

    c10

---
+++ atcbc385p1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int A, B, C;
+    scanf("%d %d %d", &A, &B, &C);
+    if ((A + B == C) || (A + C == B) || (B + C == A) || (A == B == C))
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    return 0;
+}
