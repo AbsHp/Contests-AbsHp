80d8da2a155c1e6fc11a7cbb0b7777d24341c866
AbsHp <asinghal21@iitk.ac.in>
2024-12-25 10:48:56 +0530

    atcbc43p4

---
+++ atcbc43p4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LEN 100000
+
+int is_unbalanced(const char *str, int start, int end)
+{
+    int freq[26] = {0};
+    int length = end - start + 1;
+    for (int i = start; i <= end; i++)
+    {
+        freq[str[i] - 'a']++;
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        if (freq[i] > length / 2)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    char s[MAX_LEN + 1];
+    scanf("%s", s);
+    int n = strlen(s);
+    for (int len = 2; len <= 3; len++)
+    {
+        for (int i = 0; i <= n - len; i++)
+        {
+            if (is_unbalanced(s, i, i + len - 1))
+            {
+                printf("%d %d\n", i + 1, i + len);
+                return 0;
+            }
+        }
+    }
+    printf("-1 -1\n");
+    return 0;
+}
+
+// AtCoder Beginner Contest 43 P4
