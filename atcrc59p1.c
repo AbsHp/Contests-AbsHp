13cc61328890b6b8a2a53313929e6f73ffb23b37
AbsHp <asinghal21@iitk.ac.in>
2024-12-25 10:25:27 +0530

    atcrc59p1

---
+++ atcrc59p1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    int a[N];
+    int minValue = INT_MAX, maxValue = INT_MIN;
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &a[i]);
+        if (a[i] < minValue)
+            minValue = a[i];
+        if (a[i] > maxValue)
+            maxValue = a[i];
+    }
+    int minCost = INT_MAX;
+    for (int target = minValue; target <= maxValue; target++)
+    {
+        int currentCost = 0;
+        for (int i = 0; i < N; i++)
+        {
+            int diff = a[i] - target;
+            currentCost += diff * diff;
+        }
+        if (currentCost < minCost)
+            minCost = currentCost;
+    }
+    printf("%d\n", minCost);
+    return 0;
+}
+
+// AtCoder Regular Contest 59 P1
