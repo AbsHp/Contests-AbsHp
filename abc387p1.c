8b687ea1fc09b4cd2912b81688c648130aba5a8c
AbsHp <asinghal21@iitk.ac.in>
2025-01-04 17:38:00 +0530

    abc387p1

---
+++ abc387p1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+    int sum = 0;
+    sum = A + B;
+    int result = 1;
+    result = sum * sum;
+    printf("%d", result);
+    return 0;
+}
