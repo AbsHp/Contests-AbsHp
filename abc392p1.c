d4755c5b1c0795bbe6b6987710a6cc3333db6bd6
AbsHp <asinghal21@iitk.ac.in>
2025-10-18 00:03:14 +0530

    abc392p1

---
+++ abc392p1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int A1, A2, A3;
+    scanf("%d %d %d", &A1, &A2, &A3);
+    if ((A1 *A2 == A3) || (A2 *A3 == A1) || (A3 *A1 == A2))
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    return 0;
+}
